c0a0f799cb77c5dac68108570d80104cac4e53e1
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-10-25 11:12:27 +0100

    Update VideoSplash.java

--- src/android/VideoSplash.java
+++ src/android/VideoSplash.java
@@ -1,28 +1,24 @@
 package com.example.videosplash;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.VideoView;
-import org.apache.cordova.*;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class VideoSplash extends CordovaPlugin {
-
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("show")) {
-            showVideoSplash();
-            callbackContext.success();
+            showSplash(callbackContext);
             return true;
         }
         return false;
     }
 
-    private void showVideoSplash() {
-        Activity activity = this.cordova.getActivity();
-        Intent intent = new Intent(activity, VideoSplashActivity.class);
-        activity.startActivity(intent);
+    private void showSplash(CallbackContext callbackContext) {
+        // Start the VideoSplashActivity
+        Intent intent = new Intent(cordova.getActivity(), VideoSplashActivity.class);
+        cordova.getActivity().startActivity(intent);
+        callbackContext.success(); // Call success callback
     }
 }
