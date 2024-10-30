6cf7d002c8548fc6e6607492ea53db2a09509188
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-10-30 13:07:03 +0000

    Update VideoPlayer.js

--- www/VideoPlayer.js
+++ www/VideoPlayer.js
@@ -1,24 +1,9 @@
-/*var exec = require('cordova/exec');
-
-var VideoPlayer = {
-    play: function(videoUrl, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'VideoPlayer', 'play', [videoUrl]);
-    }
-};
-
-module.exports = VideoPlayer;*/
-
 var exec = require('cordova/exec');
 
 var VideoPlayer = {
     play: function(videoUrl, successCallback, errorCallback) {
         exec(successCallback, errorCallback, 'VideoPlayer', 'play', [videoUrl]);
-    },
-    getLocalVideoUrl: function(fileName) {
-        // Ensure the Cordova File plugin is available
-        return cordova.file.applicationDirectory + 'www/' + fileName;
     }
 };
 
 module.exports = VideoPlayer;
-
