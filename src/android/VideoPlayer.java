932b0ede5c9e70002e391930745653af04720612
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-10-30 12:00:56 +0000

    Update VideoPlayer.java

--- src/android/VideoPlayer.java
+++ src/android/VideoPlayer.java
@@ -1,4 +1,4 @@
-/*package com.example.videoplayer;
+package com.example.videoplayer;
 
 import android.content.Intent;
 import org.apache.cordova.CordovaPlugin;
@@ -24,33 +24,4 @@ public class VideoPlayer extends CordovaPlugin {
         intent.putExtra(VideoPlayerActivity.EXTRA_VIDEO_URL, videoUrl);
         cordova.getActivity().startActivity(intent);
     }
-}*/
-
-package com.example.videoplayer;
-
-import android.content.Intent;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-import org.json.JSONArray;
-import org.json.JSONException;
-
-public class VideoPlayer extends CordovaPlugin {
-
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("play".equals(action)) {
-            String videoUrl = "cdvfile://localhost/www/local_video.mp4"; // Path to video in www folder
-            playVideo(videoUrl);
-            callbackContext.success();
-            return true;
-        }
-        return false;
-    }
-
-    private void playVideo(String videoUrl) {
-        Intent intent = new Intent(cordova.getActivity(), VideoPlayerActivity.class);
-        intent.putExtra(VideoPlayerActivity.EXTRA_VIDEO_URL, videoUrl);
-        cordova.getActivity().startActivity(intent);
-    }
 }
-
