8b978dcecc54c834142f0085d491297571b56166
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-10-30 13:06:33 +0000

    Update VideoPlayer.java

--- src/android/VideoPlayer.java
+++ src/android/VideoPlayer.java
@@ -1,31 +1,3 @@
-/*package com.example.videoplayer;
-
-import android.content.Intent;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-import org.json.JSONArray;
-import org.json.JSONException;
-
-public class VideoPlayer extends CordovaPlugin {
-
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("play".equals(action)) {
-            String videoUrl = args.getString(0);
-            playVideo(videoUrl);
-            callbackContext.success();
-            return true;
-        }
-        return false;
-    }
-
-    private void playVideo(String videoUrl) {
-        Intent intent = new Intent(cordova.getActivity(), VideoPlayerActivity.class);
-        intent.putExtra(VideoPlayerActivity.EXTRA_VIDEO_URL, videoUrl);
-        cordova.getActivity().startActivity(intent);
-    }
-}*/
-
 package com.example.videoplayer;
 
 import android.content.Intent;
