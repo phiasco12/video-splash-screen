42f9a995312ed553e92e07722688d463924720ea
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-10-30 16:42:09 +0000

    Update VideoPlayer.java

--- src/android/VideoPlayer.java
+++ src/android/VideoPlayer.java
@@ -1,37 +1,6 @@
-/*package com.example.videoplayer;
-
-import android.content.Intent;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-import org.json.JSONArray;
-import org.json.JSONException;
-
-public class VideoPlayer extends CordovaPlugin {
-
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("play".equals(action)) {
-            String videoUrl = args.getString(0);
-            playVideo(videoUrl);
-            callbackContext.success();
-            return true;
-        }
-        return false;
-    }
-
-    private void playVideo(String videoUrl) {
-        Intent intent = new Intent(cordova.getActivity(), VideoPlayerActivity.class);
-        intent.putExtra(VideoPlayerActivity.EXTRA_VIDEO_URL, videoUrl);
-        cordova.getActivity().startActivity(intent);
-    }
-}*/
-
-
-
 package com.example.videoplayer;
 
 import android.content.Intent;
-import android.util.Log;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
@@ -39,13 +8,10 @@ import org.json.JSONException;
 
 public class VideoPlayer extends CordovaPlugin {
 
-    private static final String TAG = "VideoPlayer";
-
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if ("play".equals(action)) {
             String videoUrl = args.getString(0);
-            Log.d(TAG, "Received video URL: " + videoUrl); // Log the video URL
             playVideo(videoUrl);
             callbackContext.success();
             return true;
