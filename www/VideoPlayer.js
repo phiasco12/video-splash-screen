f08ddfe9b445663bf57299a3064565032baadb4b
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-11-02 23:50:32 +0000

    Update VideoPlayer.js

--- www/VideoPlayer.js
+++ www/VideoPlayer.js
@@ -1,20 +1,8 @@
-/*var exec = require('cordova/exec');
+var exec = require('cordova/exec');
 
 var VideoPlayer = {
     play: function(videoUrl, successCallback, errorCallback) {
         exec(successCallback, errorCallback, 'VideoPlayer', 'play', [videoUrl]);
-    }
-};
-
-module.exports = VideoPlayer;*/
-
-
-var exec = require('cordova/exec');
-
-var VideoPlayer = {
-    play: function(videoFileName, successCallback, errorCallback) {
-        // Pass only the filename, like "local_splash_video.mp4"
-        exec(successCallback, errorCallback, 'VideoPlayer', 'play', [videoFileName]);
     }
 };
 
