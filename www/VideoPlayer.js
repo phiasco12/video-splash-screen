1210eb375d3c92bc1bcf723fa960c54def26b958
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-11-02 22:52:52 +0000

    Update VideoPlayer.js

--- www/VideoPlayer.js
+++ www/VideoPlayer.js
@@ -11,9 +11,8 @@ module.exports = VideoPlayer;*/
 var exec = require('cordova/exec');
 
 var VideoPlayer = {
-    play: function(videoUrl, successCallback, errorCallback) {
-        // Ensure that the URL passed points to a local resource
-        exec(successCallback, errorCallback, 'VideoPlayer', 'play', [videoUrl]);
+    play: function(videoPath, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'VideoPlayer', 'play', [videoPath]);
     }
 };
 
